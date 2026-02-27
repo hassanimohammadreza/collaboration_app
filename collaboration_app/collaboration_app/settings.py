05c95ca84d5f8c48bc1dab9290c28ad5f6684433
hassanimohammadreza <reza-h@att.net>
2026-02-27 10:51:02 +0330

    "postgresql"

--- collaboration_app/collaboration_app/settings.py
+++ collaboration_app/collaboration_app/settings.py
@@ -73,8 +73,12 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'collaboration_db',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- runserver.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call venv\Scripts\activate
-python -m uvicorn collaboration_app.asgi:application --reload
-pause
